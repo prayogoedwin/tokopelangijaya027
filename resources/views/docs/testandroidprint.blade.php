09046df0f36dd8eda1fcd284cc1349add946306d
AlfaNgopi <aiprstv4869@gmail.com>
2026-09-01 16:16:25 +0700

    fix: documentasi android test print

--- resources/views/docs/testandroidprint.blade.php
+++ resources/views/docs/testandroidprint.blade.php
@@ -77,11 +77,11 @@
                 
 
                 <div class="mt-6 border-t border-gray-100 dark:border-gray-700/50 pt-4">
-                    <a href="{{ route('android.print.test') }}" target="_blank" class="w-full inline-flex items-center justify-center px-4 py-2.5 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all duration-150">
+                    <a href="my.bluetoothprint.scheme://{{ route('android.print.test') }}" target="_blank" class="w-full inline-flex items-center justify-center px-4 py-2.5 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all duration-150">
                         <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"></path>
                         </svg>
-                        Uji Coba Cetak (POS-80B)
+                        Uji Coba Cetak (Bluetooth)
                     </a>
                 </div>
             </div>
